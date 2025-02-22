ece257d6696b04199ece13d40333e7b1811b9150
letsbreelhere <letsbreelhere@gmail.com>
2025-02-22 13:18:38 -0800

    Fixed up tile renderer

--- map_generator/map_renderer.gd
+++ map_generator/map_renderer.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+const TILE_WIDTH = 16
+
 @onready var tile_map: TileMapLayer = $TileMapLayer
 @onready var camera: Camera2D = $Camera2D
 @onready var map_gen = $MapGenerator
@@ -11,6 +13,7 @@ var drag_start_pos = Vector2()
 func _ready() -> void:
   $MapGenerator.generate_map()
   redraw_map()
+  camera.position = Vector2((map_gen.map_width * TILE_WIDTH) / 2.0, (map_gen.map_width * TILE_WIDTH) / 2.0)
 
 func _input(event: InputEvent) -> void:
   if event is InputEventMouseButton:
@@ -32,9 +35,9 @@ func _process(delta: float) -> void:
 func _physics_process(delta: float) -> void:
   # Get mouse position in world coordinates
   var mouse_pos = get_local_mouse_position()
-  var map_x = int(mouse_pos.x / 16)
+  var map_x = int(mouse_pos.x / TILE_WIDTH)
   map_x -= map_gen.map_width / 2
-  var map_y = int(mouse_pos.y / 16)
+  var map_y = int(mouse_pos.y / TILE_WIDTH)
   map_y -= map_gen.map_width / 2
 
   # Update tooltip text with map value
@@ -60,3 +63,4 @@ func redraw_map() -> void:
       if map_gen.altitude_value(x-0.5, y+0.5) == map_gen.TileType.LAND:
         n += 8
       tile_map.set_cell(Vector2i(ax, ay), 1, Vector2i(n, 0))
+  print(tile_map.get_used_cells_by_id(1))
